9e543e6d8bfbae9194302ff4bc8d9f3e38e54a99
Alexander Bazo <alexanderbazo@googlemail.com>
2022-06-10 13:30:07 +0200

    Prepare starter code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,12 @@
+import Task from "/resources/js/task/Task.js";
 import TaskListView from "/resources/js/ui/TaskListView.js";
-import DBConnector from "/resources/js/db/DBConnector.js";
 import Logger from "/resources/js/utils/Logger.js";
 
-var databaseConnector,
-    taskListView;
-
-async function initDatabase(strategy) {
-    databaseConnector = new DBConnector(strategy);
-    await databaseConnector.open(true);
-    return true;
-}
+var taskListView;
 
 async function initUserInterface() {
-    let tasks = await databaseConnector.getAllTasks();
+    let tasks = [];
+    // TODO Pass all known tasks from database to list view
     taskListView = new TaskListView(document.querySelector("#tasks"));
     taskListView.addEventListener("newTaskRequested", onNewTaskRequested);
     taskListView.addEventListener("taskCleanupRequested", onTaskCleanupRequested);
@@ -23,24 +17,24 @@ async function initUserInterface() {
 }
 
 async function onTaskUpdated(task) {
-    await databaseConnector.updateTask(task);
-    // Make sure that actual state from database is shown in UI
+    // TODO Update task in database before re-rendering
     taskListView.update(task);
 }
 
 async function onNewTaskRequested() {
-    let task = await databaseConnector.createTask();
+    let task = new Task();
+    // TODO Get new task from database instead
     task.addEventListener("update", (event) => onTaskUpdated(event.data));
     taskListView.add(task);
 }
 
 async function onTaskCleanupRequested(event) {
     for (let i = 0; i < event.data.length; i++) {
-        await databaseConnector.removeTask(event.data[i]);
+        // TODO Delete tasks from database before removing views from UI
         taskListView.remove(event.data[i]);
     }
 }
 
 Logger.enable();
-await initDatabase(DBConnector.INDEXED_DB_STRATEGY);
+// TODO Connect to database with selected strategy before initializing user interface
 await initUserInterface();
